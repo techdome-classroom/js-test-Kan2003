8936324a4290b951973d333f2a12d693d78970f1
Kanha Vishwakarma <122142009+Kan2003@users.noreply.github.com>
2024-01-05 13:39:59 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = []; 
+	const parenthesis = { 
+		"(": ")", 
+		"[": "]", 
+		"{": "}", 
+	}; 
+
+	for (let char of s) { 
+		if (parenthesis[char]) { 
+			stack.push(char); 
+		} else if ( 
+			char === ")" || 
+			char === "]" || 
+			char === "}"
+		) { 
+			if ( 
+				parenthesis[stack.pop()] !== 
+				char 
+			) { 
+				return false; 
+			} 
+		} 
+	} 
+
+	return stack.length === 0;
 };
 
 module.exports = { isValid };
